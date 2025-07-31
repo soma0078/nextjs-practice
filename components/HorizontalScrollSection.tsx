44f2407636f400ee9aa64507d1c23adc265aacd6
soma0078 <thddk0121@gmail.com>
2025-07-31 11:56:00 +0900

    feat: add ScrollTrigger animation

--- components/HorizontalScrollSection.tsx
+++ components/HorizontalScrollSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { cn } from "@/lib/utils";
@@ -23,16 +23,52 @@ const datas = [
 ];
 
 export default function HorizontalScrollSection() {
+  const containerRef = useRef<HTMLElement | null>(null);
+
   useEffect(() => {
-    gsap.to(".box", {
-      scrollTrigger: ".box",
-      x: 500,
+    if (!containerRef.current) return;
+
+    const boxes = gsap.utils.toArray<HTMLLIElement>(".box");
+    const lines = gsap.utils.toArray<HTMLLIElement>(".step-line");
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top top",
+        end: "+=300%",
+        scrub: true,
+        pin: true,
+      },
     });
-  });
+
+    boxes.forEach((box, i) => {
+      tl.fromTo(
+        box,
+        { autoAlpha: 0, x: 50 },
+        { autoAlpha: 1, x: 0, duration: 1 }
+      );
+      if (i < lines.length) {
+        tl.to(
+          lines[i],
+          { width: 450, duration: 1, ease: "none" },
+          i * 0.3 + 0.5
+        );
+      }
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((st) => st.kill());
+      tl.kill();
+    };
+  }, []);
 
   return (
-    <section className="bg-gray-50 py-20">
-      <ul className={cn("box", "space-y-12.5")}>
+    <section
+      className="bg-gray-50 py-20 overflow-x-auto"
+      id="box-container"
+      ref={containerRef}
+    >
+      <ul className="space-x-12.5 flex justify-between  max-w-266 mx-auto">
         {datas.map((data) => {
           return <ListItem key={data.step} {...data} />;
         })}
@@ -43,9 +79,22 @@ export default function HorizontalScrollSection() {
 
 function ListItem({ step, content }: { step: number; content: string }) {
   return (
-    <li className="odd:[&>div]:bg-gray-300 even:[&>div]:bg-gray-100 text-center [&>div]:mx-auto space-y-2">
-      <span className="size-6 rounded-full bg-blue-300 inline-flex justify-center font-semibold text-white">
+    <li
+      className={cn(
+        "box",
+        "odd:[&>div]:bg-gray-300 even:[&>div]:bg-gray-100 text-center [&>div]:mx-auto space-y-2",
+        "last:[&_.step-line]:hidden"
+      )}
+    >
+      <span className="size-6 z-5 rounded-full bg-blue-300 inline-flex justify-center relative font-semibold text-white">
         {step}
+        <span
+          className={cn(
+            "step-line",
+            "absolute top-1/2 h-0.5 w-50 bg-blue-300 -z-1 left-0"
+          )}
+          style={{ width: 0, transformOrigin: "left center" }}
+        />
       </span>
       <h3>{content}</h3>
       <div className="size-50 ">image </div>
